4e5cad4ef0b5b7868e9f2afc453e6974c9c83dad
Ayrton Itiro Kobo Yoshii <itiro_yoshii@hotmail.com>
2024-05-14 19:50:21 -0300

    Finish 01-node-turorial; Add 02-express-turtorial

---
+++ 01-node-tutorial/17-http-stream.js
@@ -0,0 +1,20 @@
+var http = require('http')
+var fs = require('fs')
+
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+
+    // reading data in chunks and also writing it in chunks
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+      res.end(err)
+    })
+  })
+  .listen(5000)
+
+  
